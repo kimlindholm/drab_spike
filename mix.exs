e1ab26bc83887f6c3a66e597cec744ed5ea31e62
Kim Lindholm <misc@dynevision.com>
2017-10-26 16:12:12 +0700

    Upgrade to Elixir 1.5 and add aliases to Mixfile

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DrabSpike.Mixfile do
     [
       app: :drab_spike,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
@@ -54,7 +54,9 @@ defmodule DrabSpike.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      "test": ["ecto.create --quiet", "ecto.migrate", "test"],
+      "s": ["phx.server"],
+      "r": ["phx.routes"]
     ]
   end
 end
